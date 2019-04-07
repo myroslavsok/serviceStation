ee99a9dce3d5c7f547632752dcf1f0bb84ea621b
beztormoza <beztormoza@ukr.net>
2019-04-07 23:36:16 +0300

    Fix release Heroku bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyCor3ORnTDJkEqU6biD3OhgVdVg9rPQUMA",
     authDomain: "service-station-is-db.firebaseapp.com",
