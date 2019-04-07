dbf47f93a55c0a436d92996238d0e1d91f68154f
beztormoza <beztormoza@ukr.net>
2019-04-07 23:40:32 +0300

    Fix release Heroku bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCor3ORnTDJkEqU6biD3OhgVdVg9rPQUMA",
+    authDomain: "service-station-is-db.firebaseapp.com",
+    databaseURL: "https://service-station-is-db.firebaseio.com",
+    projectId: "service-station-is-db",
+    storageBucket: "service-station-is-db.appspot.com",
+    messagingSenderId: "57271506950"
+  }
 };
