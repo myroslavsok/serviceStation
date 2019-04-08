6c8a0de7da19df4ac8ddf7f6ecb8417a9524999a
beztormoza <beztormoza@ukr.net>
2019-04-08 23:55:02 +0300

    Can start server on localhost

--- server.js
+++ server.js
@@ -5,17 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/service-station-is'));
-// app.use(express.static('/dist/service-station-is'));
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join('/dist/service-station-is/index.html'));
-// });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => {
-  console.log('server started');
+app.use(express.static(__dirname + '/dist/service-station-IS'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/service-station-IS/index.html'));
 });
 
-app.use(express.static('/dist/service-station-IS'));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/service-station-IS/index.html'));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080, () => {
+  console.log('log: server started');
 });
