4a1f0d7d0a0b42f3cc169f8c3e26334e616f98e5
beztormoza <beztormoza@ukr.net>
2019-04-08 22:15:58 +0300

    Fix release Heroku bugs

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static('./dist/service-station-is'));
-app.use(express.static('/dist/service-station-IS'));
+// app.use(express.static('/dist/service-station-is'));
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join('/dist/service-station-is/index.html'));
+// });
+app.use(express.static('/hz'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join('/dist/service-station-IS/index.html'));
+  res.sendFile(path.join('/hz'));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
